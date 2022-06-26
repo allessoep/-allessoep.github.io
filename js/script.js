407850bb511759151517e2deb38e823c3f76390b
allessoep <57958153+allessoep@users.noreply.github.com>
2022-06-26 14:10:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 //desktop//
-document.getElementById( "logoimage" ).onclick= function(){
-let str = document.getElementById("sidebartitel").innerHTML;
+document.getElementById( "lightbutton" ).onclick= function(){
 document.getElementById("sidebartitel").innerHTML =  "javascript test";
+document.getElementById("sidebartekst").innerHTML =  "<p>◢ javascript test</p> <p>◢ javascipt test2</p>";
 };
 
+document.getElementById( "lightbutton" ).onclick= function(){
+document.getElementById("sidebartitel").innerHTML =  "javascript test";
+document.getElementById("sidebartekst").innerHTML =  "<p>◢ javascript test</p> <p>◢ javascipt test2</p>";
+};
 
 //mobile//
-document.getElementById("artimage").ontouchstart = function() {
+document.getElementById("logoimage").ontouchstart = function() {
   element = document.querySelector('#sidebarcontent');
   element.style.visibility = 'visible';
 };
